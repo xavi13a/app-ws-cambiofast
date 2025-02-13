104ba90a34df96d9a98f7936c2280bc6f7fe2a30
camilo.medina <camilo.medina@farmaciasperuanas.pe>
2025-02-13 12:37:43 -0500

    despliegue GCP

--- src/main/java/com/pichincha/money/service/JwtService.java
+++ src/main/java/com/pichincha/money/service/JwtService.java
@@ -10,44 +10,29 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final SecretKey SECRET_KEY;
     private final long expirationTime;
     private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     
-    public JwtService(@Value("${app.jwt.secret}") String secret, 
-                      @Value("${app.jwt.expiration}") long expiration) {
-        this.SECRET_KEY = Keys.hmacShaKeyFor(secret.getBytes());
+    public JwtService(@Value("${app.jwt.expiration}") long expiration) {
         this.expirationTime = expiration;
     }
-
-    // Generar JWT de sesión
-//    public String generateSessionToken(String email) {
-//        return Jwts.builder()
-//                .setSubject(email)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-//                .signWith(secretKey)
-//                .compact();
-//    }
     
     public Mono<String> generateSessionToken(String email) {
         return Mono.fromSupplier(() -> {
             return Jwts.builder()
                 .setSubject(email)
-                .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(secretKey)
                 .compact();
         });
     }
 
-    // Validar JWT de sesión
     public String validateSessionToken(String token) {
         try {
             return Jwts.parserBuilder()
@@ -57,7 +42,7 @@ public class JwtService {
                     .getBody()
                     .getSubject();
         } catch (Exception e) {
-            return null; // Token inválido
+            return null;
         }
     }
     
